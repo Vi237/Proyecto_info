cf2b33d9a4c69b2fb4b8d394353c89ecec50d685
Vilma Cero Sandoval <vilma6969diqsi@gmail.com>
2021-08-26 12:39:52 -0400

    Toolbar

--- app/src/main/java/com/vi/infogram/CreatAccountActivity.java
+++ app/src/main/java/com/vi/infogram/CreatAccountActivity.java
@@ -1,6 +1,7 @@
 package com.vi.infogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,12 @@ public class CreatAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creat_account);
+        sowToolBar(getString(R.string.title_crear_cuenta),true);
+    }
+    public void sowToolBar(String titulo,boolean botonSubir){
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
     }
 }
